0c5b5e72fb5ea5442ffb24f28f63d1ed36778bd1
kawacho <kawacho@gmail.com>
2018-06-15 16:10:11 +0900

    P.39 - P.41 Franc クラスも Money クラス派生に
    amount フィールドと equals メソッドは親クラスにあるのと同一なので削除。

--- src/money/Franc.java
+++ src/money/Franc.java
@@ -1,20 +1,13 @@
 package money;
 
-public class Franc {
+public class Franc extends Money {
 	
-	private int amount;
-
 	public Franc(int amount) {
 		this.amount = amount;
 	}
 
 	Franc times(int multiplier) {
 		return new Franc(this.amount * multiplier);
-	}
-	
-	public boolean equals(Object object) {
-		Franc franc = (Franc) object;
-		return this.amount == franc.amount;
 	}
 
 }
