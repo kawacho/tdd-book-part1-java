db7f1dc643944403951549c62616f746c846e781
kawacho <kawacho@gmail.com>
2018-06-18 11:02:24 +0900

    P.106 Money#times の返値の型も Expression に

--- src/money/Money.java
+++ src/money/Money.java
@@ -10,7 +10,7 @@ public class Money implements Expression {
 		this.currency = currency;
 	}
 	
-	Money times(int multiplier) {
+	Expression times(int multiplier) {
 		return new Money(this.amount * multiplier, this.currency);
 	}
 	
